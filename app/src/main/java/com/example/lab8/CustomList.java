48b603d2032b0af720a58de3ed110c38d9224ba8
Zach North <znorth@ualberta.ca>
2024-03-06 16:18:15 -0700

    Adding hasCityTest

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -60,5 +60,8 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
+    public boolean hasCity(City city){
+        return Boolean.FALSE;
+    }
 
 }
--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,8 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -14,12 +16,21 @@ public class CustomListTest {
 
     private CustomList list;
     /**
-     * create a mock list for my city list
-     * @return
+     * create a mock list for my city list with one city by default
+     * @return the list
      */
     public CustomList MockCityList(){
         list = new CustomList(null,new ArrayList<>());
+        list.addCity(mockCity());
         return list;
+    }
+
+    /** Baseline city for tests.
+     *
+     * @return city
+     */
+    private City mockCity() {
+        return new City("Edmonton", "Alberta");
     }
     /**
      * get the size of the list
@@ -33,5 +44,12 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
+    }
+    @Test
+    void hasCityTest(){
+        list = MockCityList();
+        assertTrue(list.hasCity(mockCity()));
+        City city = new City("Charlottetown", "Prince Edward Island");
+        assertFalse(list.hasCity(city));
     }
 }
