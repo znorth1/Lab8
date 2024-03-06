252122bc5d633c78ae7a38d4cc0c4994943f803b
Zach North <znorth@ualberta.ca>
2024-03-06 16:04:31 -0700

    Test will work now

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -54,10 +54,11 @@ public class CustomList extends ArrayAdapter<City> {
     }
     /**
      * this adds a city object to the list
-     *for the first phase it will be
-     empty * @param city
+     *the second phase, you can add the
+     city * @param city
      */
     public void addCity(City city){
+        cities.add(city);
     }
 
 }
